0d970536385480a1f7a739dabb51f762b1df5a24
Jorge Betancur <jorge.betancur@teads.tv>
2024-09-13 17:07:00 -0500

    refactor(users): delted user valiadtion

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,12 +3,11 @@ import { getAllUsers, getUserById } from '../controllers/userController';
 import { authorizeRoles } from '../middlewares/authorizationMiddleware';
 import { authenticateToken } from '../middlewares/authMiddleware';
 import { RoleType } from '@prisma/client';
-import { validateTenantData } from '../controllers/tenantController';
 
 const router = Router();
 
 router.get('/', authenticateToken, authorizeRoles([RoleType.OWNER]), getAllUsers);
-router.get('/:id', authenticateToken, authorizeRoles([RoleType.OWNER]), validateTenantData, getUserById);
+router.get('/:id', authenticateToken, authorizeRoles([RoleType.OWNER]), getUserById);
 
 // Añadir rutas para crear, actualizar, eliminar usuarios
 
